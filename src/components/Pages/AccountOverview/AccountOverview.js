9d3fe93056d82a7075cb3cad89e75f22e167297a
Dax <dpatel@marinerfinance.com>
2021-11-19 08:45:33 -0500

    Fixed Formatting

--- src/components/Pages/AccountOverview/AccountOverview.js
+++ src/components/Pages/AccountOverview/AccountOverview.js
@@ -12,71 +12,51 @@ import ActiveLoans from "./ActiveLoans";
 import RecentPayments from "./RecentPayments";
 import "./Style.css";
 import usrAccountDetails from "../../Controllers/AccountOverviewController";
-import LimitedOffer from "./LimitedOffer"
+import LimitedOffer from "./LimitedOffer";
 
 export default function AccountOverview() {
   const classes = useStylesAccountOverview();
-
   //API Call
   const [accountDetails, setAccountDetails] = useState(null);
   async function getUserAccountDetails() {
     setAccountDetails(await usrAccountDetails());
   }
-
   useEffect(() => {
     getUserAccountDetails();
   }, []);
 
   //Load data
-  let offerData = (accountDetails != null) ? accountDetails.data.data.offerData : null;
-  let applicationsData = (accountDetails != null) ? accountDetails.data.data.applicants : null;
-  let status = (accountDetails != null) ? accountDetails.data.status : null;
-  let activeLoansData = (accountDetails != null) ? accountDetails.data.data.activeLoans : null;
-  let recentPaymentData = (accountDetails != null) ? accountDetails.data.data.activeLoans : null;
+  let offerData = accountDetails?.data?.data?.offerData;
+  let applicationsData = accountDetails?.data?.data?.applicants;
+  let status = accountDetails?.data?.status;
+  let activeLoansData = accountDetails?.data?.data?.activeLoans;
+  let recentPaymentData = accountDetails?.data?.data?.activeLoans;
 
   //View
   return (
     <div>
       <ScrollToTopOnMount />
       <CheckLoginStatus />
-      <Grid
-        container
-        justifyContent="center"
-        style={{
-          marginTop: "-150px",
-          paddingRight: "30px",
-          paddingLeft: "30px",
-        }}
-      >
-
+      <Grid container justifyContent="center" style={{ marginTop: "-150px", paddingRight: "30px", paddingLeft: "30px", }}>
         <Grid item xs={12} style={{ width: "100%" }} container direction="row">
           <Typography variant="h5" className={classes.heading} data-testid="subtitle">
             Account Overview
           </Typography>
         </Grid>
         <LimitedOffer userOffers={offerData} />
-
         <Grid item xs={12} style={{ width: "100%", paddingTop: "30px" }} container direction="row">
           <Typography variant="h5" className={classes.subheading} data-testid="subtitle">
             Summary of applications
           </Typography>
         </Grid>
         <RecentApplications userApplicationsData={applicationsData} UserAccountStatus={status} />
-
-
         <Grid item xs={12} style={{ width: "100%" }} container direction="row">
           <Typography variant="h5" className={classes.subheading} data-testid="subtitle">
             Active Loan
           </Typography>
         </Grid>
         <ActiveLoans userActiveLoanData={activeLoansData} />
-
-
-        <Grid
-          item
-          xs={12}
-          style={{ width: "100%", padding: "5px", paddingTop: "20px", paddingBottom: "25px" }}
-        >
+        <Grid item xs={12} style={{ width: "100%", padding: "5px", paddingTop: "20px", paddingBottom: "25px" }}>
           <Paper className={classes.paper} style={{ height: "85%" }}>
             <Grid container spacing={3}>
               <Grid item xs={12} sm={6}>
@@ -85,10 +65,7 @@ export default function AccountOverview() {
                 </Typography>
               </Grid>
               <Grid item xs={12} sm={6}>
-                <NavLink
-                  to="/customers/paymenthistory"
-                  style={{ textDecoration: "none" }}
-                >
+                <NavLink to="/customers/paymenthistory" style={{ textDecoration: "none" }}>
                   <ButtonPrimary stylebutton='{"float": "right", "color":"" }'>
                     Payment History
                   </ButtonPrimary>
@@ -96,7 +73,6 @@ export default function AccountOverview() {
               </Grid>
             </Grid>
             <RecentPayments userRecentPaymentData={recentPaymentData} />
-
           </Paper>
         </Grid>
       </Grid>
