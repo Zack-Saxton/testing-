2270b4322c55a83e06d13d25afa7c7062c7afa22
Arun Krishnamoorthy <arun.k@zucisystems.com>
2022-01-19 20:28:12 +0530

    worked on card no 1126,1116,1032

--- src/components/Pages/AccountOverview/AccountOverview.js
+++ src/components/Pages/AccountOverview/AccountOverview.js
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useStylesAccountOverview } from "./Style";
 import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import { ButtonPrimary } from "../../FormsUI";
-import { NavLink } from "react-router-dom";
 import CheckLoginStatus from "../../App/CheckLoginStatus";
 import ScrollToTopOnMount from "../ScrollToTop";
 import RecentApplications from "./RecentApplications";
@@ -74,61 +71,14 @@ export default function AccountOverview() {
             Account Overview
           </Typography>
         </Grid>
+
+        {/* ****************components************ */}
+        
         <LimitedOffer userOffers={offerData} />
-
-        <Grid
-          item
-          xs={12}
-          style={{ width: "100%", paddingTop: "30px" }}
-          container
-          direction="row"
-        >
-          <Typography
-            variant="h5"
-            className={classes.subheading}
-            data-testid="subtitle"
-          >
-            Summary of applications
-          </Typography>
-        </Grid>
-        <RecentApplications
-          userApplicationsData={applicationsData}
-          UserAccountStatus={status}
-          userApplicantData={applicantData}
-        />
-
-        <Grid
-          item
-          xs={12}
-          style={{ width: "100%", paddingBottom: "10px", paddingTop: "10px" }}
-          container
-          direction="row"
-        >
-          <Typography style={{ padding: "0" }} variant="h5" className={classes.subheading} data-testid="subtitle" >
-            Active Loan
-          </Typography>
-        </Grid>
         <ActiveLoans userActiveLoanData={activeLoansData} />
-
-        <Grid item xs={12} style={{ width: "100%", padding: "0px 0px 40px 0px" }}>
-          <Paper id="recentPaymentsWrap" className={classes.paper} style={{ height: "85%" }} >
-            <Grid container spacing={3}>
-              <Grid item xs={12} sm={6}>
-                <Typography variant="h5" className={classes.activeLoanHeading}>
-                  Recent Payments
-                </Typography>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <NavLink to="/customers/paymenthistory" style={{ textDecoration: "none" }}>
-                  <ButtonPrimary stylebutton='{"float": "right","padding":"0px 30px", "fontSize":"0.938rem","fontFamily":"Muli,sans-serif" }'>
-                    Payment History
-                  </ButtonPrimary>
-                </NavLink>
-              </Grid>
-            </Grid>
-            <RecentPayments userRecentPaymentData={recentPaymentData} />
-          </Paper>
-        </Grid>
+        <RecentApplications userApplicationsData={applicationsData} UserAccountStatus={status} userApplicantData={applicantData} />
+        <RecentPayments userRecentPaymentData={recentPaymentData} />
+          
       </Grid>
     </div>
   );
