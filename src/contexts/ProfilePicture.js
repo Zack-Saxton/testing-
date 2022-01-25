03fda7811774f5fa18538954ac4da340b59f4024
Alex Peguero-Cruz <apcruz@marinerfinance.com>
2022-01-25 14:43:56 -0500

    elint applied

--- src/contexts/ProfilePicture.js
+++ src/contexts/ProfilePicture.js
@@ -1,22 +1,29 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState } from 'react';
+
 export const ProfilePicture = createContext();
 
-const ProfilePictureContext = (props) => {
-	const [dataProfile, setData] = useState({
-		profile_picture_url: "",
-	});
+function ProfilePictureContext(props) {
+  const [dataProfile, setData] = useState({
+    profile_picture_url: '',
+  });
 
-	const resetProfilePicture = () => {
-		setData({
-			profile_picture_url: "",
-		})
-	}
+  const resetProfilePicture = () => {
+    setData({
+      profile_picture_url: '',
+    });
+  };
 
-	return (
-		<ProfilePicture.Provider value={{ dataProfile: dataProfile, setData: setData, resetProfilePicture: resetProfilePicture }}>
-			{props.children}
-		</ProfilePicture.Provider>
-	);
-};
+  return (
+    <ProfilePicture.Provider
+      value={{
+        dataProfile,
+        setData,
+        resetProfilePicture,
+      }}
+    >
+      {props.children}
+    </ProfilePicture.Provider>
+  );
+}
 
 export default ProfilePictureContext;
