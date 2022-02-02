e344ff28521a2b84b2d6db850a38a78988e39d33
vickeykgv <vignesh.kg@zucisystems.com>
2022-02-03 00:36:51 +0530

    fixed console logs

--- src/components/FormsUI/TableCell/index.js
+++ src/components/FormsUI/TableCell/index.js
@@ -16,7 +16,7 @@ const TableCellWrapper = ({ parseData, className, align, ...otherProps }) => {
 		var dom_content = [];
 		for (var key in data.data) {
 			dom_content.push(
-				<TableCell
+				<TableCell key={key}
 					className={ data.data[ key ].className }
 					align={ data.data[ key ].align }
 				>
@@ -30,9 +30,9 @@ const TableCellWrapper = ({ parseData, className, align, ...otherProps }) => {
 	const BuildTable = (data) => {
 		var dom_content = [];
 		if (data) {
-			data.data.forEach(function (arrayItem) {
+			data.data.forEach(function (arrayItem, index) {
 				dom_content.push(
-					<TableRow>
+					<TableRow key={index}>
 						<BuildCell data={ arrayItem } />
 					</TableRow>
 				);
