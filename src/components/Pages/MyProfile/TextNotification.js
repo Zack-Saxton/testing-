74f910dc1cb3b2c704464e1f05f16c8ef2b125f7
Arun Krishnamoorthy <arun.k@zucisystems.com>
2022-02-02 14:08:17 +0530

    1045 loadin issue fixed

--- src/components/Pages/MyProfile/TextNotification.js
+++ src/components/Pages/MyProfile/TextNotification.js
@@ -37,6 +37,7 @@ export default function TextNotification() {
   let phone = Cookies.get("opted_phone_texting");
   let textnotifybool = Cookies.get("isTextNotify") === "true" ? true : false;
   let [ disabledContent, setdisabledContent ] = useState(textnotifybool);
+  
   const onClickCancelChange = () => {
     formikTextNote.resetForm();
     history.push({ pathname: "/customers/myProfile" });
@@ -57,6 +58,9 @@ export default function TextNotification() {
       .oneOf([ true ], "You must accept the terms and conditions")
       .oneOf([ false ], "False You must accept the terms and conditions"),
   });
+
+
+  
 
   const formikTextNote = useFormik({
     initialValues: {
@@ -79,16 +83,11 @@ export default function TextNotification() {
         if (result.data?.sbt_subscribe_details?.HasNoErrors === true || result.data?.sbt_getInfo?.HasNoErrors === true) {
           toast.success("Updated successfully");
           Cookies.set("isTextNotify", disabledContent);
-          window.setTimeout(function () {
-            window.location.reload();
-          }, 4000);
+           onClickCancelChange()
         } else {
           toast.error("No changes made");
         }
-        window.setTimeout(function () {
-          setLoading(false);
-          setdisabledContent(true);
-        }, 3050);
+          onClickCancelChange()
       } catch (error) {
         ErrorLogger("Error occured while changing text notification.", error);
       }
