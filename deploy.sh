2bf1dd8f26981050759ec73871827a08dcf905ba
Alex Peguero-Cruz <apcruz@marinerfinance.com>
2022-02-02 15:03:49 -0500

    updates

--- deploy.sh
+++ deploy.sh
@@ -124,7 +124,7 @@ echo "**************************************************************************
 
 latestCommit=$(git rev-parse --short HEAD)
 #Dockerise the environment
-#imageName="marinerfinance/ops:${app}-${env}-${latestCommit}"
+imageName="marinerfinance/ops:${app}-${env}-${latestCommit}"
 #docker build -f Dockerfile -t ${imageName} .
 case $env in
   "qa")
@@ -135,6 +135,7 @@ case $env in
     fi
     ;;
   "dev")
+    cat ~/.
     docker build -t $imageName  $(for i in `cat ~/.env_cac_dev`; do out+="--build-arg $i " ; done; echo $out;out="") .
     if [ $? != 0 ]; then
       echo -e "\033[1;31m Failed \033[0m => (reason): docker failed  to build image locally"
@@ -193,57 +194,57 @@ echo -e "\033[1;36m ********************************************** \033[0m"
 exit
 
 #ssh -o "StrictHostKeyChecking no" -i $PEM_FILE $serverName << ENDHERE
-# ssh  -i $_PEM_FILE_ $server << ENDHERE
-#     echo -e "\033[1;36m ********************************************** \033[0m"
-#     echo -e "\033[1;36m * START stopping all container ($app) and then  \033[0m"
-#     echo -e "\033[1;36m * removing all  container ($app)                \033[0m"
-#     echo -e "\033[1;36m ********************************************** \033[0m"
-#     removeAllContainer=\$(docker ps -aq)
-#     echo ${removeAllContainer}
-#     docker rm -f \$removeAllContainer
-#     echo -e "\033[1;36m ********************************************** \033[0m"
-#     echo -e "\033[1;36m * removed all running container from : ($app)  \033[0m"
-#     echo -e "\033[1;36m ********************************************** \033[0m"
-#     echo -e "\033[1;36m ********************************************** \033[0m"
-#     echo -e "\033[1;36m * START removing all old images : ($app)       \033[0m"
-#     echo -e "\033[1;36m ********************************************** \033[0m"
-#     removeAllImages=\$(docker images -aq)
-#     echo ${removeAllImages}
-#     docker rmi \$removeAllImages
-#     echo -e "\033[1;36m ********************************************** \033[0m"
-#     echo -e "\033[1;36m * removed all images from : ($app)             \033[0m"
-#     echo -e "\033[1;36m ********************************************** \033[0m"
-#     echo -e "\033[1;36m ********************************************** \033[0m"
-#     echo -e "\033[1;36m * START Updating server    : ($app)            \033[0m"
-#     echo -e "\033[1;36m ********************************************** \033[0m"
+ssh  -i $_PEM_FILE_ $server << ENDHERE
+     echo -e "\033[1;36m ********************************************** \033[0m"
+     echo -e "\033[1;36m * START stopping all container ($app) and then  \033[0m"
+     echo -e "\033[1;36m * removing all  container ($app)                \033[0m"
+     echo -e "\033[1;36m ********************************************** \033[0m"
+     removeAllContainer=\$(docker ps -aq)
+     echo ${removeAllContainer}
+     docker rm -f \$removeAllContainer
+     echo -e "\033[1;36m ********************************************** \033[0m"
+     echo -e "\033[1;36m * removed all running container from : ($app)  \033[0m"
+     echo -e "\033[1;36m ********************************************** \033[0m"
+     echo -e "\033[1;36m ********************************************** \033[0m"
+     echo -e "\033[1;36m * START removing all old images : ($app)       \033[0m"
+     echo -e "\033[1;36m ********************************************** \033[0m"
+     removeAllImages=\$(docker images -aq)
+     echo ${removeAllImages}
+     docker rmi \$removeAllImages
+     echo -e "\033[1;36m ********************************************** \033[0m"
+     echo -e "\033[1;36m * removed all images from : ($app)             \033[0m"
+     echo -e "\033[1;36m ********************************************** \033[0m"
+     echo -e "\033[1;36m ********************************************** \033[0m"
+     echo -e "\033[1;36m * START Updating server    : ($app)            \033[0m"
+     echo -e "\033[1;36m ********************************************** \033[0m"
 
-#     sudo apt-get update && sudo apt-get dist-upgrade -y
+     sudo apt-get update && sudo apt-get dist-upgrade -y
 
-#     docker login --username=$DOCKERHUB_USER  --password=$DOCKERHUB_PSWD
+     docker login --username=$DOCKERHUB_USER  --password=$DOCKERHUB_PSWD
 
-#     if [ $? != 0 ]; then
-#         echo -e "\033[1;31m Failed \033[0m => (reason): docker failed  to login to dockerHub"
-#         exit 1;
-#     fi
+     if [ $? != 0 ]; then
+         echo -e "\033[1;31m Failed \033[0m => (reason): docker failed  to login to dockerHub"
+         exit 1;
+     fi
 
-#   for ((count=1;count<=$instances;count++))
-#   do
-#     echo  "****** Spinning Instance "\$count": "
-#     docker run -dit --restart=always --name "${app}"\$count"-${env}-${latestCommit}" --network $dockerNetwork $imageName
-#     sleep 5
-#   done
-#   exit
-# ENDHERE
+   for ((count=1;count<=$instances;count++))
+   do
+     echo  "****** Spinning Instance "\$count": "
+     docker run -dit --restart=always --name "${app}"\$count"-${env}-${latestCommit}" --network $dockerNetwork $imageName
+     sleep 5
+   done
+   exit
+ENDHERE
 
-# # docker inspect -f '{{json .NetworkSettings.Networks}}' ${app}"\$count"-${env}-${latestCommit} | python -m json.tool
-# # curl_cmd=$(curl -w "\n" --insecure -X GET https://${app}-${env}.marinerfinance.io)
+ # docker inspect -f '{{json .NetworkSettings.Networks}}' ${app}"\$count"-${env}-${latestCommit} | python -m json.tool
+ # curl_cmd=$(curl -w "\n" --insecure -X GET https://${app}-${env}.marinerfinance.io)
 
-# # echo -e "\033[1;36m ********************************************** \033[0m"
-# # echo -e "\033[1;36m * verifying that ${app} is up via API  \033[0m"
-# # echo -e "\033[1;36m ********************************************** \033[0m"
+ # echo -e "\033[1;36m ********************************************** \033[0m"
+ # echo -e "\033[1;36m * verifying that ${app} is up via API  \033[0m"
+ # echo -e "\033[1;36m ********************************************** \033[0m"
 
-# # echo -e "\033[1;32m *  $curl_cmd  \033[0m"
+ # echo -e "\033[1;32m *  $curl_cmd  \033[0m"
 
-# message="$app Deployment END from $branch to $env By $deployUser"
-# url="https://hooks.slack.com/services/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"
-# curl -X POST -H 'Content-type: application/json' --data '{"text":"'"$message"'"}' "{$url}"
+message="$app Deployment END from $branch to $env By $deployUser"
+url="https://hooks.slack.com/services/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"
+curl -X POST -H 'Content-type: application/json' --data '{"text":"'"$message"'"}' "{$url}"
