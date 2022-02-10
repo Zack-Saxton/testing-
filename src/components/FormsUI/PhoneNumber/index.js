445480b8e6bdddacf5e34ad2b2afa27f882a86f2
Dax <dpatel@marinerfinance.com>
2022-02-09 19:28:34 -0500

    Fixed Phone Number

--- src/components/FormsUI/PhoneNumber/index.js
+++ src/components/FormsUI/PhoneNumber/index.js
@@ -18,17 +18,15 @@ const PhoneNumberWrapper = ({ name, onChange, value, label, error, disabled, hel
   //Set Formik field
   // const [field, mata] = useField(name);
   const [ unmaskedval, setUnMaskedVal ] = useState(value);
-
+  const [ setValues ] = useState("");
   const handleChange = (event) => {
-  
     if (onChange) {
       onChange(event);
     }
-    var x = event.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
-  event.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
-  
+    let x = event.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+    event.target.value = !x[ 2 ] ? x[ 1 ] : '(' + x[ 1 ] + ') ' + x[ 2 ] + (x[ 3 ] ? '-' + x[ 3 ] : '');
     setUnMaskedVal(event.target.value);
-   
+    setValues(event.target.value);
   };
 
   return (
@@ -38,7 +36,7 @@ const PhoneNumberWrapper = ({ name, onChange, value, label, error, disabled, hel
         <InputMask
           style={ { width: "100%" } }
           mask="(999) 999-9999"
-          value={value}
+          value={ value }
           name={ name }
           onChange={ handleChange }
           data-test-id="phone"
@@ -47,7 +45,7 @@ const PhoneNumberWrapper = ({ name, onChange, value, label, error, disabled, hel
         >
           { () => <TextField label={ label }
             name={ name }
-            value={value}
+            value={ value }
             error={ error }
             placeholder="Enter Phone Number"
             helperText={ helperText }
