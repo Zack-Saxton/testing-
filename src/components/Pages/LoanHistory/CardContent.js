f537cf544f321453ebf676674948da47439c9bfb
Arun Krishnamoorthy <arun.k@zucisystems.com>
2022-02-17 18:57:55 +0530

    1322

--- src/components/Pages/LoanHistory/CardContent.js
+++ src/components/Pages/LoanHistory/CardContent.js
@@ -2,17 +2,33 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import AccountBalanceWalletIcon from "@material-ui/icons/AccountBalanceWallet";
 import MonetizationOnRoundedIcon from "@material-ui/icons/MonetizationOnRounded";
-import React from "react";
+import React,{useState,useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import { useStylesLoanHistory } from "./Style";
 import "./Style.css";
+import { useQuery } from "react-query";
+import usrAccountDetails from "../../Controllers/AccountOverviewController";
+import applicationStatusRedirectPage from "../../../assets/data/applicationStatusRedirectPage.json"
 
 export default function LoanHistoryCard(historyOfLoans) {
   window.zeHide();
   const navigate = useNavigate();
   //Material UI css class
   const classes = useStylesLoanHistory();
-  //Loan history data from API
+
+  const { data: dataAccountOverview } = useQuery('loan-data', usrAccountDetails);
+  const [ checkPresenceOfLoan, setCheckPresenceOfLoan ] = useState(false);
+  const [ checkPresenceOfLoanStatus, setCheckPresenceOfLoanStatus ] = useState('');
+
+  useEffect(() => {
+  let activeLoan = dataAccountOverview?.data?.applicants;
+
+  const presenceOfLoan = activeLoan?.some((applicant) => applicant.isActive === true && applicant?.status !== "referred" && applicant?.status !== "contact_branch");
+    const presenceOfLoanStatus = activeLoan?.find((applicant) => applicant.isActive === true);
+
+  setCheckPresenceOfLoanStatus(presenceOfLoanStatus?.status);
+    setCheckPresenceOfLoan(presenceOfLoan);
+  }, [ dataAccountOverview ]);
 
   const redirectToApplyForLoan = () => {
     navigate("/customers/applyForLoan", { state: { from: "user" } });
@@ -20,6 +36,11 @@ export default function LoanHistoryCard(historyOfLoans) {
   const redirectToMakeAPayment = () => {
     navigate("/customers/makePayment", { state: { from: "user" } });
   };
+  const redirectToResumeApplication = () => {
+    navigate(applicationStatusRedirectPage[ checkPresenceOfLoanStatus ], { state: { from: "user" } });
+
+  };
+
   //  view part
   return (
     <Grid item xs={ 12 } style={ { paddingBottom: "20px", paddingTop: "10px" } }>
@@ -50,7 +71,16 @@ export default function LoanHistoryCard(historyOfLoans) {
               </Grid>
             </Paper>
           </Grid>
-
+          { checkPresenceOfLoan === true ?
+          <Grid item xs={ 12 } sm={ 4 } className={ classes.cardLoanHistory }>
+            <Paper className={ classes.paperPointer } onClick={ redirectToResumeApplication } style={ { height: "70%" } }>
+              <Grid style={ { textAlign: "center" } }>
+                <MonetizationOnRoundedIcon id="dolor-icon_loan-history" className="material-icons background-round mt-5 yelloWBG" />
+                <p className={ classes.cardApplyLoan }>Resume Application</p>
+              </Grid>
+            </Paper>
+          </Grid>
+          :
           <Grid item xs={ 12 } sm={ 4 } className={ classes.cardLoanHistory }>
             <Paper className={ classes.paperPointer } onClick={ redirectToApplyForLoan } style={ { height: "70%" } }>
               <Grid style={ { textAlign: "center" } }>
@@ -58,9 +88,10 @@ export default function LoanHistoryCard(historyOfLoans) {
                 <p className={ classes.cardApplyLoan }>Apply for a Loan</p>
               </Grid>
             </Paper>
-          </Grid>
+          </Grid> }
         </Grid>
       </Paper>
     </Grid>
   );
 }
+
