ad6407900d4773f5de66d56a5d548a97670cfb38
vickeykgv <vignesh.kg@zucisystems.com>
2022-03-15 23:03:12 +0530

    UIUX-1500-MissingErrorMsgInDocumentUpload

--- src/components/Pages/ApplyLoan/Stepper/BankAccountVerification.js
+++ src/components/Pages/ApplyLoan/Stepper/BankAccountVerification.js
@@ -72,6 +72,7 @@ export default function BankAccountVerification(props) {
 	const [ paymnetMode, setPaymentMode ] = useState("autopayment");
 	const [ verifyRequired, setVerifyRequired ] = useState(false);
 	const [ error, setError ] = useState("");
+	const [ fileUploadSuccess, setFileUploadSuccess ] = useState(false);
 	const [ invalidRN, setInvalidRN ] = useState(false);
 	const [ openAutoPayAuth, setOpenAutoPayAuth ] = useState(false);
 	function getElementByText(text, ctx) {
@@ -81,9 +82,11 @@ export default function BankAccountVerification(props) {
 	const handleUpload = (res) => {
 		if (res?.data?.bank_account_verification) {
 			toast.success(messages?.document?.uploadSuccess);
+			setFileUploadSuccess(true);
 			props.next();
 			getElementByText("Income Verification").scrollIntoView();
 		} else {
+			props.setLoadingFlag(false);
 			toast.error(messages?.document?.upoloadFailed);
 		}
 	};
@@ -109,6 +112,11 @@ export default function BankAccountVerification(props) {
 				bank_name: values.bankInformation,
 				repayment: paymnetMode,
 			};
+			if(verifyRequired && !fileUploadSuccess){
+				toast.error("please upload the document");
+				props.setLoadingFlag(false);
+			}
+			else{
 			let res = await APICall("bank_information_cac", '', data, "POST", true);
 			if (res?.data?.bank_account_information && res?.data?.bank_account_verification) {
 				props.setLoadingFlag(false);
@@ -128,6 +136,7 @@ export default function BankAccountVerification(props) {
 				props.setLoadingFlag(false);
 				alert("Network Error");
 			}
+		}
 		},
 	});
 
