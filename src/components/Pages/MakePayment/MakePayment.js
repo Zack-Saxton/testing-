22b735d08eab533470ae23331b31fb5e8fc954f4
John Elder <jelder@marinerfinance.comgit config --global user.email jelder@marinerfinance.com>
2022-03-22 09:57:04 -0400

    fixed trim

--- src/components/Pages/MakePayment/MakePayment.js
+++ src/components/Pages/MakePayment/MakePayment.js
@@ -321,7 +321,7 @@ export default function MakePayment(props) {
 
   //Select account
   const handleChangeSelect = (event) => {
-    setCard(event.target.value.trim());
+    setCard(event.target.value);
     if (event.nativeEvent.target.innerText.includes("Checking") || event.nativeEvent.target.innerText.includes("Savings")) {
       setIsDebit(false);
       setCheckCard(false);
@@ -498,7 +498,7 @@ export default function MakePayment(props) {
 
   //payment onblur
   const onBlurPayment = (event) => {
-    let price = event.target.value.trim().replace("$", "");
+    let price = event.target.value.replace("$", "");
     price = Number(price).toFixed(2);
     setPaymentAmount(price);
     setRequiredAmount("");
