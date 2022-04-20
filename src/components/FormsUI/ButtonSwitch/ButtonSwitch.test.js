aeaca1215afdb3cc912966891f09f34e5e2a77b5
ashok2maruthamuthu <ashok.maruthamuthu@zucisystems.com>
2022-04-20 17:58:46 +0530

    Unit testing for ButtonSwitch and Button with Icon

---
+++ src/components/FormsUI/ButtonSwitch/ButtonSwitch.test.js
@@ -0,0 +1,59 @@
+import "@testing-library/jest-dom";
+import { fireEvent, render } from "@testing-library/react";
+import React from "react";
+import ButtonSwitch from "./index";
+
+const handleChange = jest.fn();
+const component = (enableFlag = false) =>{  
+  return (<ButtonSwitch
+    onChange={ handleChange }    
+    disabled = { enableFlag }
+    data-testid="switch"
+    >
+    Continue
+    </ButtonSwitch>);
+};
+
+test("Check the Switch Button Availability", ()=>{
+  let container =  render(component());
+  const element = container.getByTestId('switch');
+  expect(element).toBeTruthy();
+});
+
+test("Check the Switch is not disabled", ()=>{
+  let container =  render(component());
+  const input = container.getByTestId('switch');
+  expect(input).not.toHaveAttribute('disabled');
+});
+
+test("Check the Switch is disabled", ()=>{
+  let container =  render(component(true));
+  const input = container.getByTestId('switch');
+  expect(input).not.toHaveAttribute('disabled');
+});
+
+test("By default the switch to be ON", ()=>{
+  let container =  render(component());
+  const input = container.getByTestId('switch');
+  expect(input).toBeChecked();
+});
+
+test("The switch to be OFF in click event", ()=>{
+  let container =  render(component());
+  const input = container.getByTestId('switch');
+  fireEvent.click(input);
+  expect(input).not.toBeChecked();
+});
+
+test("The switch to be OFF and ON when click two time", ()=>{
+  let container =  render(component());
+  const input = container.getByTestId('switch');
+  fireEvent.click(input);
+  fireEvent.click(input);
+  expect(input).toBeChecked();
+});
+
+test('should match the snapshot', () => {
+  const { asFragment } = render(component());
+  expect(asFragment).toMatchSnapshot();
+});
--- src/components/FormsUI/ButtonWithIcon/ButtonWithIcon.test.js
+++ src/components/FormsUI/ButtonWithIcon/ButtonWithIcon.test.js
@@ -3,23 +3,8 @@ import React from 'react';
 import ButtonWithIcon from './index.js';
 
 afterEach(cleanup);
-
-test("renders icon", () => {
-    const container = render(
-        <ButtonWithIcon
-            icon="close"
-            iconposition="left"
-            stylebutton='{"background": "", "color":"" }'
-            styleicon='{ "color":"" }'
-        >
-            Cancel
-        </ButtonWithIcon>);
-    const input = container.getByTestId('icon');
-    expect(input).toBeTruthy();
-});
-
-test('should match the snapshot', () => {
-    const { asFragment } = render(<ButtonWithIcon
+let component = () => {
+    return ( <ButtonWithIcon
         icon="close"
         iconposition="left"
         stylebutton='{"background": "", "color":"" }'
@@ -27,5 +12,14 @@ test('should match the snapshot', () => {
     >
         Cancel
     </ButtonWithIcon>);
+}
+test("renders icon", () => {
+    const container = render(component());
+    const input = container.getByTestId('icon');
+    expect(input).toBeTruthy();
+});
+
+test('should match the snapshot', () => {
+    const { asFragment } = render(component());
     expect(asFragment).toMatchSnapshot();
 });
