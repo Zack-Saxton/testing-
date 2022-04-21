01f0fb42b2a570cb0610c3e6c2ae9771e0398240
ashok2maruthamuthu <ashok.maruthamuthu@zucisystems.com>
2022-04-21 21:51:01 +0530

    Unit test automation for text field with icon and zip code

--- src/components/FormsUI/TextfieldWithIcon/TextFieldIcon.test.js
+++ src/components/FormsUI/TextfieldWithIcon/TextFieldIcon.test.js
@@ -1,56 +1,41 @@
-import { cleanup, render } from '@testing-library/react';
+import { cleanup, render, fireEvent } from '@testing-library/react';
 import React from 'react';
 import TextFieldWithIcon from './index.js';
 
 afterEach(cleanup);
+const component = () =>{
+    return(<TextFieldWithIcon
+        name="userName1"
+        label="Enter Username"
+        icon="cloud"
+        iconColor="#595E6E"
+        iconPosition="right"
+        materialProps={ { "data-testid": "test" } }
+        required={ true }
+    />);
+}
 
-test("renders icon", () => {
-    const container = render(
-        <TextFieldWithIcon
-            name="userName1"
-            label="Enter Username"
-            icon="cloud"
-            iconColor="#595E6E"
-            iconPosition="right"
-            required={ true }
-        />);
-    const input = container.getByTestId('icon');
-    expect(input).toBeTruthy();
-});
-
-test("renders input field", () => {
-    const container = render(
-        <TextFieldWithIcon
-            name="userName1"
-            label="Enter Username"
-            icon="cloud"
-            iconColor="#595E6E"
-            iconPosition="right"
-            required={ true }
-            materialProps={ { "data-test-id": "test" } }
-        />);
+test("Check the text filed is render", () => {
+    const container = render(component());
     const input = container.getByTestId('test');
     expect(input).toBeTruthy();
 });
 
-test('should match the snapshot', () => {
-    const { asFragment } = render(<TextFieldWithIcon
-        name="userName1"
-        label="Enter Username"
-        icon="cloud"
-        iconColor="#595E6E"
-        iconPosition="right"
-        required={ true }
-        materialProps={ { "data-test-id": "test" } }
-    />);
+test("Check the text filed is render with Icon", () => {
+    const container = render(component());
+    const input = container.getByTestId('icon');
+    expect(input).toBeTruthy();
+});
 
-    expect(asFragment(<TextFieldWithIcon
-        name="userName1"
-        label="Enter Username"
-        icon="cloud"
-        iconColor="#595E6E"
-        iconPosition="right"
-        required={ true }
-        materialProps={ { "data-test-id": "test" } }
-    />)).toMatchSnapshot();
+test("Check can able to enter value", () => {
+    const container = render(component());
+    const input = container.getByTestId('test');
+    fireEvent.change(input, { target: { value: "Mariner" } });
+    expect(input.value).toBe('Mariner');
+});
+  
+
+test('should match the snapshot', () => {
+    const { asFragment } = render(component());
+    expect(asFragment).toMatchSnapshot();
 });
--- src/components/FormsUI/ZipCode/Zipcode.test.js
+++ src/components/FormsUI/ZipCode/Zipcode.test.js
@@ -1,18 +1,18 @@
-import { cleanup, fireEvent, render } from '@testing-library/react';
+import { cleanup, fireEvent, render, } from '@testing-library/react';
 import React from 'react';
 import Zipcode from './index.js';
 
 afterEach(cleanup);
-
-test('Availability test', () => {
-  const container = render(
-    <Zipcode
-      type="text"
-      name="text"
-      label="test"
-      materialProps={ { "data-test-id": "zipcode" } }
-    />);
-
+const component = () => {
+  return(<Zipcode
+    type="text"
+    name="text"
+    label="test"
+    materialProps={ { "data-testid": "zipcode" } }
+  />);
+}
+test('Check the input filed is rendered', () => {
+  const container = render(component());
   const input = container.getByTestId('zipcode');
   expect(input).toBeTruthy();
   expect(input.value).toBe('');
@@ -20,29 +20,16 @@ test('Availability test', () => {
 
 });
 
-test('Input test', () => {
-  const container = render(
-    <Zipcode
-      type="text"
-      name="text"
-      label="test"
-      materialProps={ { "data-test-id": "zipcode" } }
-    />);
+test('Check can able to enter value', () => {
+  const container = render(component());
 
   const input = container.getByTestId('zipcode');
   fireEvent.change(input, { target: { value: "123" } });
   expect(input.value).toBe('123');
 });
 
-test('Get only numeric value', () => {
-  const container = render(
-    <Zipcode
-      type="text"
-      name="text"
-      label="test"
-      materialProps={ { "data-test-id": "zipcode", maxLength: 10 } }
-    />);
-
+test('Check input field is allowing only numeric value ', () => {
+  const container = render(component());
   const input = container.getByTestId('zipcode');
   fireEvent.change(input, { target: { value: "abc" } });
   expect(input.value).toBe('');
@@ -50,24 +37,13 @@ test('Get only numeric value', () => {
   expect(input.value).toBe('123');
 });
 
-it('should be 5 digits', () => {
-  const wrapper = render(<Zipcode
-    type="text"
-    name="text"
-    label="test"
-    materialProps={ { "data-test-id": "zipcode" } }
-  />);
+it('Check the max length of input, it should be 5 digits', () => {
+  const wrapper = render(component());
   const input = wrapper.getByTestId('zipcode');
   expect(input.maxLength).toBe(5);
 });
 
-test('should match the snapshot', () => {
-  const { asFragment } = render();
-
-  expect(asFragment(<Zipcode
-    type="text"
-    name="text"
-    label="test"
-    materialProps={ { "data-test-id": "zipcode" } }
-  />)).toMatchSnapshot();
+test('Should match the snapshot', () => {
+  const { asFragment } = render(component());
+  expect(asFragment).toMatchSnapshot();
 });
