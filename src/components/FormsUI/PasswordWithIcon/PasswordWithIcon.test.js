c877d20940ada833c152ffc0d2f25c6b2b419bab
Deepika Asokan <deepika.a@zucisystems.com>
2022-04-25 18:21:49 +0530

    UIUX-1787-Unit Testing- Password with icon

--- src/components/FormsUI/PasswordWithIcon/PasswordWithIcon.test.js
+++ src/components/FormsUI/PasswordWithIcon/PasswordWithIcon.test.js
@@ -1,9 +1,8 @@
-import { cleanup, render } from '@testing-library/react';
+import { cleanup, fireEvent, render } from '@testing-library/react';
 import React from 'react';
 import PasswordWithIcon from './index.js';
 
 afterEach(cleanup);
-
 const component = () => {
   return (
     <PasswordWithIcon
@@ -11,19 +10,35 @@ const component = () => {
       label="Enter Username"
       icon="lock"
       iconColor="#595E6E"
-      iconPosition="left"
+      iconPosition="right"
+      materialProps={{ "data-testid": "test" }}
+      required={true}
     />
   )
 }
 
-test("renders icon", () => {
+test("Check the Password Field is rendering", () => {
+  const container = render(component());
+  const input = container.getByTestId('test');
+  expect(input).toBeTruthy();
+});
+
+test("Check the Password Field is rendered with Icon", () => {
   const container = render(component());
   const input = container.getByTestId('icon');
   expect(input).toBeTruthy();
-})
+});
 
-test("renders icon", () => {
+test("Check can able to enter value", () => {
   const container = render(component());
-  const input = container.getByTestId('passProps');
-  expect(input).toBeTruthy();
-})
+  const input = container.getByTestId('test');
+  fireEvent.change(input, { target: { value: "Mariner" } });
+  expect(input.value).toBe('Mariner');
+});
+
+
+test('should match the snapshot', () => {
+  const { asFragment } = render(component());
+  expect(asFragment).toMatchSnapshot();
+});
+
