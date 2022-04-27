38c4c2d4a1832bde3c2340a5276accd58e22fc40
Prakash Chandrasekar <prakash.c@zucisystems.com>
2022-04-28 00:38:37 +0530

    UIUX-1678-multi-factor-UI-prakash

---
+++ src/components/Pages/MultiFactorAuthentication/MultiFactorAuthentication.js
@@ -0,0 +1,90 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import { useStylesMFA } from "./Style";
+import "./MultiFactorAuthentication.css";
+import { ButtonPrimary } from "../../FormsUI";
+import {
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  Typography,
+} from "@mui/material";
+
+const MultiFactorAuthentication = () => {
+  const classes = useStylesMFA();
+  const securityCode = (
+    <div className={classes.securityCodeText}>
+      Security code via SMS :<br />
+      <span>Get a code on (***) *** 2376</span>
+    </div>
+  );
+  const securityQuestions = (
+    <div className={classes.securityQuestions}>
+      Security Questions : <br />
+      Answer security questions{" "}
+    </div>
+  );
+
+  return (
+    <div>
+      <Grid className="twoStepWrap">
+        <Grid
+          spacing={1}
+          container
+          item
+          xs={12}
+          sm={12}
+          md={6}
+          lg={6}
+          xl={6}
+          className={classes.twoStepWrap}
+        >
+          <Paper className={classes.twoStepPaper}>
+            <Typography className={classes.twoStepHeading} variant="h5">
+              2-Step Verification
+            </Typography>
+            <Typography className={classes.twoStepParagraph}>
+              For your security we are asking you to verify your identity.
+              Select one of the following methods to complete your login.
+            </Typography>
+
+            <FormControl
+              className={classes.radioButtonwrap}
+              component="fieldset"
+            >
+              <RadioGroup
+                className={classes.radioGroupWrap}
+                id="textAndCallss"
+                aria-label="method"
+                name="method"
+                row={true}
+              >
+                <FormControlLabel
+                  id="FormControlLabel"
+                  className={classes.smallRadioButton}
+                  value="T"
+                  control={<Radio color="primary" />}
+                  label={securityCode}
+                />
+                <FormControlLabel
+                  id="FormControlLabel"
+                  className={classes.smallRadioButton}
+                  value="M"
+                  control={<Radio color="primary" />}
+                  label={securityQuestions}
+                />
+              </RadioGroup>
+            </FormControl>
+            <Grid className={classes.nextButtonGrid} container>
+              <ButtonPrimary stylebutton='{"color":""}'>Next</ButtonPrimary>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default MultiFactorAuthentication;
---
+++ src/components/Pages/MultiFactorAuthentication/Style.js
@@ -0,0 +1,96 @@
+import { makeStyles, createStyles } from "@mui/styles";
+
+//Styling Part
+const useStylesMFA = makeStyles((theme) =>
+  createStyles({
+    smallRadioButton: {
+      margin: "10px 0px",
+      "& svg": {
+        width: "20px !important",
+        height: "20px !important",
+      },
+    },
+    twoStepWrap: {
+      margin: "auto",
+      maxWidth: "630px",
+      height: "88vh",
+      alignItems: "center",
+      width: "100%",
+    },
+    headingTextWrap: {
+      position: "relative",
+    },
+    backArrow: {
+      position: "absolute",
+      left: "0",
+      top: "0",
+      padding: "4px 0px",
+      width: "32px",
+      height: "32px",
+    },
+    yellowBackArrow: {
+      color: "#ffbc23",
+    },
+    twoStepPaper: {
+      padding: "20px 40px",
+      // margin:"0px 10px",
+      boxSizing: "border-box",
+      height: "fit-content",
+      boxShadow: "rgb(61 61 61 / 10%) 0px 8px 44px 0px",
+      "@media (max-width: 699px)": {
+        margin: "0px 30px",
+      },
+      "@media (max-width: 420px)": {
+        padding: "20px 10px",
+      },
+    },
+    twoStepHeading: {
+      textAlign: "center",
+      fontSize: "1.538rem",
+      marginTop: "0.82rem",
+      color: "#171717",
+    },
+    twoStepParagraph: {
+      width: "100%",
+      textAlign: "center",
+      margin: "15px 0px",
+      fontSize: "0.938rem",
+      color: "#595959",
+    },
+    securityCodeText: {
+      fontSize: "0.938rem",
+      color: "#595959",
+    },
+    securityQuestions: {
+      fontSize: "0.938rem",
+      color: "#595959",
+    },
+    radioButtonwrap: {
+      width: "100%",
+      padding: "0px",
+      boxSizing: "border-box",
+    },
+    radioGroupWrap: {
+      display: "flex",
+      flexDirection: "column",
+    },
+    nextButtonGrid: {
+      justifyContent: "center",
+      margin: "15px 0px",
+    },
+    otpWrap: {
+      marginBottom: "25px",
+    },
+    otpNumber: {
+      padding: "0px 5px",
+      textAlign: "center",
+    },
+    resetText: {
+      fontSize: "0.844rem",
+      textAlign: "center",
+      margin: "30px 0px 0px 0px",
+    },
+  })
+);
+
+export { useStylesMFA };
