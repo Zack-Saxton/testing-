de4f1e3cf2e19dc7311a1b188a513d7bea97c29c
Deepika Asokan <deepika.a@zucisystems.com>
2022-05-18 22:10:59 +0530

    UIUX-1886 - Randomize Security Questin

--- src/components/Pages/MultiFactorAuthentication/MFA-SecurityQuestions.js
+++ src/components/Pages/MultiFactorAuthentication/MFA-SecurityQuestions.js
@@ -5,16 +5,14 @@ import IconButton from "@mui/material/IconButton";
 import Paper from "@mui/material/Paper";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import { ButtonPrimary, Select } from "../../FormsUI";
+import { ButtonPrimary } from "../../FormsUI";
 import "./MultiFactorAuthentication.css";
 import { useStylesMFA } from "./Style";
-import { fetchQuestionMFA, saveSecurityAnswer } from "../../Controllers/MFAController";
+import { saveSecurityAnswer } from "../../Controllers/MFAController";
 import { useFormik } from "formik";
 import Cookies from "js-cookie";
 import { toast } from "react-toastify";
-
 import * as yup from "yup";
-
 
 //Yup validations for all the input fields
 const validationSchema = yup.object({
@@ -23,79 +21,67 @@ const validationSchema = yup.object({
     .trim()
     .max(30, "Should be maximum of 30 characters")
     .required("Enter your answer"),
-  selectSecurityQuestion: yup
-    .string("Please select a security question")
-    .max(70, "Maximum of 70")
-    .required("Please select a security question"),
 });
-
 
 const MFASecurityQuestions = () => {
   const classes = useStylesMFA();
   let location = useLocation();
   const navigate = useNavigate();
-  let questionArray = [];
-  // const { questions, setQuestions } = useState([]); 
-  const [ questions, setQuestions ] = useState([]);
+  const [selectedQuestions, setSelectedQuestions] = useState();
 
-    //Form Submission
-    const formik = useFormik({
-      enableReinitialize: true,
-      initialValues: {
-        selectSecurityQuestion : "",
-        answer : ""
-      },
-  
-      validationSchema: validationSchema,
-      onSubmit: async (values) => {
-        const userEmail = Cookies.get("email");
+  //Form Submission
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      answer: "",
+    },
 
-        let answerData = {
-          email: userEmail,
-          deviceType: navigator.userAgent,
-          securityQuestions: [
-              {question_id: values.selectSecurityQuestion, answer: values.answer} 
-          ]
-        }
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      const userEmail = Cookies.get("email");
 
-        let verify = await saveSecurityAnswer(answerData);
-        if(verify?.data?.hasError === false  && verify?.data?.result === "Ok")
-        {
-          toast.success(verify?.data?.Message);
-          const tokenString = Cookies.get("token") ? Cookies.get("token") : '{ }';
-          let userToken = JSON.parse(tokenString);
-          userToken.isMFACompleted = true;
-          Cookies.set("token",JSON.stringify(userToken));
-          navigate("/customers/accountoverview")
-        }
-        else if (verify?.data?.hasError === true && verify?.data?.result === "error")
-        {
-          toast.error(verify?.data?.Message);
-        }
-        else {
-          toast.error("Network error");
-        }
-      },
-    });
-  const getMFAQuestion  = async () => {
-    location?.state?.mfaSecurityQuestions?.mfaDetails?.securityQuestions.forEach(element => {
-      questionArray.push(
-        {
-          "label": element.question, 
-          "value": element.question_id
-        }
-      )
-    });
-setQuestions(questionArray)
-  }
+      let answerData = {
+        email: userEmail,
+        deviceType: navigator.userAgent,
+        securityQuestions: [
+          {
+            question_id: selectedQuestions?.question_id,
+            answer: values.answer,
+          },
+        ],
+      };
+
+      let verify = await saveSecurityAnswer(answerData);
+      if (verify?.data?.hasError === false && verify?.data?.result === "Ok") {
+        toast.success(verify?.data?.Message);
+        const tokenString = Cookies.get("token") ? Cookies.get("token") : "{ }";
+        let userToken = JSON.parse(tokenString);
+        userToken.isMFACompleted = true;
+        Cookies.set("token", JSON.stringify(userToken));
+        navigate("/customers/accountoverview");
+      } else if (
+        verify?.data?.hasError === true &&
+        verify?.data?.result === "error"
+      ) {
+        toast.error(verify?.data?.Message);
+      } else {
+        toast.error("Network error");
+      }
+    },
+  });
+  const getMFAQuestion = () => {
+    let question =
+      location?.state?.mfaSecurityQuestions?.mfaDetails?.securityQuestions;
+    let questionArray = question[Math.floor(Math.random() * question?.length)];
+    setSelectedQuestions(questionArray);
+  };
 
   const backToVerificationStep = () => {
     navigate(-1);
-  }
+  };
 
   useEffect(() => {
-
-    getMFAQuestion()
+    getMFAQuestion();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -112,79 +98,53 @@ setQuestions(questionArray)
           className={classes.twoStepWrap}
         >
           <Paper className={classes.twoStepPaper}>
-          <form onSubmit={formik.handleSubmit}>
-            <Grid className={classes.headingTextWrap}>
-              <Typography className={classes.twoStepHeading} variant="h5">
-                Security Questions
-              </Typography>
-              <Typography className={classes.securityCubText} variant="h6">
-                Answers are not case sensitive
-              </Typography>
-              <IconButton className={classes.backArrow} onClick={ backToVerificationStep }>
-                <ArrowBackIcon className={classes.yellowBackArrow} />
-              </IconButton>
-            </Grid>
-            
-            <Grid className={classes.otpWrap} container>
-            <Grid
-                className={classes.securityQuestionsInput}
-                item
-                xs={12}
-                sm={12}
-                md={12}
-                lg={12}
-              >
-                <Select
-                          id="selectSecurityQuestion"
-                          name="selectSecurityQuestion"
-                          labelform="Select security question"
-                          value={formik.values.selectSecurityQuestion}
-                          onChange={formik.handleChange}
-                          // refId={refCitizenship}
-                          onBlur={formik.handleBlur}
-                          error={(formik.touched.selectSecurityQuestion && Boolean(formik.errors.selectSecurityQuestion)) }
-                          helperText={ formik.touched.selectSecurityQuestion && formik.errors.selectSecurityQuestion }
-                          // select='[{"label": "USA Citizen", "value": "USA Citizen"},
-                          //         {"label": "Permanent Resident", "value": "Permanent Resident"},
-                          //         {"label": "Foreign Resident", "value": "Foreign Resident"}]'
-                          select = { questions ? JSON.stringify(questions) : '[]' }
-                          // select = { JSON.stringify(questions)  }
-                        />
-              </Grid>
-              <Grid
-                className={classes.securityQuestionsInput}
-                item
-                xs={12}
-                sm={12}
-                md={12}
-                lg={12}
-              >
-                <TextField
-                  id="Answer"
-                  name="answer"
-                  label="Answer"
-                  type="text"
-                  variant="standard"
-                  fullWidth
-                  value={formik.values.answer}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={formik.touched.answer && Boolean(formik.errors.answer)}
-                  helperText={formik.touched.answer && formik.errors.answer}
-                />
+            <form onSubmit={formik.handleSubmit}>
+              <Grid className={classes.headingTextWrap}>
+                <Typography className={classes.twoStepHeading} variant="h5">
+                  Security Questions
+                </Typography>
+                <Typography className={classes.securityCubText} variant="h6">
+                  Answers are not case sensitive
+                </Typography>
+                <IconButton
+                  className={classes.backArrow}
+                  onClick={backToVerificationStep}
+                >
+                  <ArrowBackIcon className={classes.yellowBackArrow} />
+                </IconButton>
               </Grid>
 
-              
-
-              
-
-              
-            </Grid>
-            <Grid className={classes.nextButtonGrid} container>
-              <ButtonPrimary stylebutton='{"color":""}' type="submit">
-                Verify Now
-              </ButtonPrimary>
-            </Grid>
+              <Grid className={classes.otpWrap} container>
+                <Grid
+                  className={classes.securityQuestionsInput}
+                  item
+                  xs={12}
+                  sm={12}
+                  md={12}
+                  lg={12}
+                >
+                  <TextField
+                    id="Answer"
+                    name="answer"
+                    label={selectedQuestions?.question}
+                    type="text"
+                    variant="standard"
+                    fullWidth
+                    value={formik.values.answer}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={
+                      formik.touched.answer && Boolean(formik.errors.answer)
+                    }
+                    helperText={formik.touched.answer && formik.errors.answer}
+                  />
+                </Grid>
+              </Grid>
+              <Grid className={classes.nextButtonGrid} container>
+                <ButtonPrimary stylebutton='{"color":""}' type="submit">
+                  Verify Now
+                </ButtonPrimary>
+              </Grid>
             </form>
           </Paper>
         </Grid>
