e3a680f3c6ea3ae3994795f1e3da8b0150a8742b
Prakash Chandrasekar <prakash.c@zucisystems.com>
2022-06-01 23:28:13 +0530

    UIUX-1918-unit-test-make-a-payment-availability-test

---
+++ src/components/Pages/MakePayment/MakePayment.test.js
@@ -0,0 +1,49 @@
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/styles";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter } from "react-router-dom";
+import MakePayment from "./MakePayment";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      staleTime: 500000,
+    },
+  },
+});
+const theme = createTheme();
+
+const component = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <MakePayment />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
+};
+
+test("Checks the component is rendered", () => {
+  render(component());
+  const element = screen.getByTestId("makePaymentComponent");
+  expect(element).toBeTruthy();
+});
+
+test("Table Availability test", () => {
+  render(component());
+  const element = screen.getByTestId("activeLoans");
+  expect(element).toBeTruthy();
+});
+
+test("Please Contact Text Availability test", () => {
+  render(component());
+  const element = screen.getByTestId("pleaseContact");
+  expect(element).toBeTruthy();
+});
