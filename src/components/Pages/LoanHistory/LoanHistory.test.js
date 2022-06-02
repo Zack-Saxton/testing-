864179f88dccb9d464c4b7e68139eda9f37ab9f7
Uma2401 <59562868+Uma2401@users.noreply.github.com>
2022-06-02 23:50:18 +0530

    mock data added

---
+++ src/components/Pages/LoanHistory/LoanHistory.test.js
@@ -0,0 +1,44 @@
+import { createTheme, StyledEngineProvider } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/styles';
+import "@testing-library/jest-dom/extend-expect";
+import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter } from "react-router-dom";
+import LoanHistory from "./LoanHistory";
+
+
+const theme = createTheme();
+window.scrollTo = jest.fn();
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			retry: false,
+			staleTime: 500000,
+		},
+	},
+});
+
+const component = () => {
+	return (
+		<ThemeProvider theme={theme}>
+			<StyledEngineProvider injectFirst>
+				<QueryClientProvider client={queryClient}>
+					<BrowserRouter>
+						<LoanHistory />
+					</BrowserRouter>
+				</QueryClientProvider>
+			</StyledEngineProvider>
+		</ThemeProvider>
+	);
+}
+test("Checks the component is rendered", () => {
+    render(component());
+    const element = screen.getByTestId("loan_history_component");
+    expect(element).toBeTruthy();
+  });
+test('should match the snapshot', () => {
+	const { asFragment } = render(component());
+	expect(asFragment).toMatchSnapshot();
+});
