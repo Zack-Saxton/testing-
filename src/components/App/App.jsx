2969c50eb45dc287b8e6ccc6eee79e77560525ba
vikas <vidnani@marinerfinance.com>
2022-06-03 15:50:18 -0400

    Updated changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -70,33 +70,6 @@ import RegisterPage from '../Pages/Register/Register';
 import VantageScore from "../Pages/VantageScore/VantageScore";
 import MFAGetPhoneNumber from "../Pages/MultiFactorAuthentication/MFAGetPhoneNumber";
 import "./App.css";
-import {useState,useEffect} from 'react'
-import axios from 'axios';
-
-function AppLocation() {
-    //creating IP state
-    const [ip, setIP] = useState('');
-  
-    //creating function to load ip address from the API
-    const getData = async () => {
-      const res = await axios.get('https://geolocation-db.com/json/')
-      console.log(res.data);
-      setIP(res.data.IPv4)
-    }
-    
-    useEffect( () => {
-      //passing getData method to the lifecycle method
-      getData()
-  
-    }, [])
-  
-    return (
-      <div className="App">
-        <h2>Your IP Address is</h2>
-        <h4>{ip}</h4>
-      </div>
-    );
-  }
 
 const queryClient = new QueryClient({
     defaultOptions: {
