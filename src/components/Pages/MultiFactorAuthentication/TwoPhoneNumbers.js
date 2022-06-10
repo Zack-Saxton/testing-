c543184fb9dc45c5dedfae1b00dbba82ab379af0
Uma2401 <59562868+Uma2401@users.noreply.github.com>
2022-06-10 20:21:49 +0530

    Null issue handled

--- src/components/Pages/MultiFactorAuthentication/TwoPhoneNumbers.js
+++ src/components/Pages/MultiFactorAuthentication/TwoPhoneNumbers.js
@@ -27,7 +27,7 @@ const TwoPhoneNumbers = ({cellPhoneNumber, optionalPhoneNumber, mfaPhoneNumber, 
     setValue(event.target.value);
   };
   const handlePopUp = () => {
-    (mfaPhoneNumber !== " ") ? setMfaPhoneNumberPopUp(true) : (cellPhoneNumber === optionalPhoneNumber) ? setSinglePhoneNumberPopUp(true) : setPopUp(true);
+    (mfaPhoneNumber !== " " && mfaPhoneNumber !== null) ?  setMfaPhoneNumberPopUp(true): (cellPhoneNumber === optionalPhoneNumber) ? setSinglePhoneNumberPopUp(true) : setPopUp(true);
 	};
   const handlePopUpClose = () => {
     setPopUp(false);
