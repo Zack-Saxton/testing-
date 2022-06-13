dec56aa7b94b69b95b217dc5b6dd3221390d5b0c
Yasin Elhilo <yelhilo@marinerfinance.com>
2022-06-13 16:27:48 -0400

    modified the unit testing for optimal coverage.

--- src/components/Pages/VantageScore/KeyFactor.test.js
+++ src/components/Pages/VantageScore/KeyFactor.test.js
@@ -1,12 +1,15 @@
-import { createTheme } from '@mui/material/styles';
-import { ThemeProvider } from '@mui/styles';
+import { Sync } from "@mui/icons-material";
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/styles";
 import "@testing-library/jest-dom/extend-expect";
 //import {jest} from "jest";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
-import VantageScore from "./VantageScore";
+import KeyFactors from "./KeyFactors";
+import VantageScore from './VantageScore'
+
 
 const theme = createTheme();
 window.scrollTo = jest.fn();
@@ -20,32 +23,126 @@ const queryClient = new QueryClient({
   },
 });
 
+let KeyFactorsMock = {
+  vantage_score: "666",
+  reason1: "98",
+  reason2: "47",
+  reason3: "14",
+  reason4: "12",
+  first_name: "SARAH",
+  last_name: "DDUPZ",
+  middle_initial: null,
+  address_city: "ARLINGTON",
+  address_state: "VA",
+  address_street: "5550 Columbia Pike",
+  address_postal_code: "22204",
+  score_change: "NoChange",
+};
+
+  
+
 it("Key Factors Component is loading in vantage Score", () => {
   render(
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
-          <VantageScore>
-            <keyFactors />
-          </VantageScore>
+          <KeyFactors keyFactors={KeyFactorsMock} />
         </BrowserRouter>
       </QueryClientProvider>
     </ThemeProvider>
-  )
-  expect(screen.getByTestId('keyfactors')).toBeInTheDocument();
-})
+  );
+
+  expect(screen.getByTestId("keyfactors")).toBeInTheDocument();
+});
 
 it("Key Factors Component is loading in Grid", () => {
   render(
     <ThemeProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <VantageScore>
-            <keyFactors />
-          </VantageScore>
-        </BrowserRouter>
-      </QueryClientProvider>
-    </ThemeProvider>
-  )
-  expect(screen.getByTestId('keyfactors-loading')).toBeInTheDocument();
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <VantageScore>
+            <KeyFactors keyFactors={KeyFactorsMock} />
+            </VantageScore>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </ThemeProvider>
+  );
+  expect(screen.getByTestId("keyfactors-loading")).toBeInTheDocument();
+});
+
+
+it('Checking if first accordion rendering and functions', async() => {
+  const { container } = render(  
+  <ThemeProvider theme={theme}>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <KeyFactors keyFactors={KeyFactorsMock} />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </ThemeProvider>);
+
+    expect(screen.getByText("There is a bankruptcy on your credit report")).toBeVisible(); 
+   
+    const button = container.querySelector(".MuiAccordionSummary-expandIconWrapper");
+    fireEvent.click(button); 
+    
+   expect(await screen.findByText(`Make all future payments on time. The impact on your credit score from the bankruptcy will diminish over time.`, { exact: false })).toBeVisible();
+})
+
+
+it('Checking if second accordion rendering and functions', async() => {
+  const { container } = render(  
+  <ThemeProvider theme={theme}>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <KeyFactors keyFactors={KeyFactorsMock} />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </ThemeProvider>);
+    expect(screen.getByText("No open bankcard or revolving accounts in your credit file")).toBeVisible(); 
+    
+    const button = container.querySelector(".MuiAccordionSummary-expandIconWrapper");
+    fireEvent.click(button); 
+      
+})
+
+
+
+it('Checking if third accordion rendering and functions', async() => {
+  const { container } = render(  
+  <ThemeProvider theme={theme}>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <KeyFactors keyFactors={KeyFactorsMock} />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </ThemeProvider>);
+
+   
+
+    expect(screen.getByText("Lack of sufficient credit history")).toBeVisible(); 
+    
+    const button = container.querySelector(".MuiAccordionSummary-expandIconWrapper");
+    fireEvent.click(button);
+
+   
+}) 
+
+
+it('Checking if forth accordion rendering and functions', async() => {
+  const { container } = render(  
+  <ThemeProvider theme={theme}>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <KeyFactors keyFactors={KeyFactorsMock} />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </ThemeProvider>);
+
+
+    expect(screen.getByText("The date that you opened your oldest account is too recent")).toBeVisible(); 
+    
+    const button = container.querySelector(".MuiAccordionSummary-expandIconWrapper");
+    fireEvent.click(button);
+   
 })
