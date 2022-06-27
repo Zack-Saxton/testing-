e0e443ed040b6d323a9496bc06fdd3df98023e33
Deepika Asokan <deepika.a@zucisystems.com>
2022-06-27 21:37:48 +0530

    UIUX-2096-Remove Skip Message from MfaPhone number

--- src/components/Pages/MultiFactorAuthentication/MFAGetPhoneNumber.js
+++ src/components/Pages/MultiFactorAuthentication/MFAGetPhoneNumber.js
@@ -20,6 +20,8 @@ import { SavePhoneNumber,fetchQuestionMFA } from "./../../Controllers/MFAControl
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import IconButton from "@mui/material/IconButton";
 import "./mfa.css";
+import CheckLoginTimeout from '../../Layout/CheckLoginTimeout';
+import CheckLoginStatus from '../../App/CheckLoginStatus';
 
 
 //Yup validation schema
@@ -41,9 +43,18 @@ const MFAGetPhoneNumber = ({
 }) => {
 	const classes = useStylesMFA();
 	const navigate = useNavigate();
+	const loginToken = JSON.parse(Cookies.get("token") ? Cookies.get("token") : '{ }');
 	const [disabledButton, setDisabledButton] = useState(true);
 	const [phoneNumber, SetPhoneNumber] = useState("");
 	const location = useLocation();
+
+	useEffect(() => {
+		if (!location?.state) {
+			navigate("/customers/accountOverview");
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
 	const preventSpace = (event) => {
 		if (event.keyCode === 32) {
 			event.preventDefault();
@@ -60,6 +71,7 @@ const MFAGetPhoneNumber = ({
 		}
 	}
 	const handleToSaveContinue = async () => {
+		if (location?.state) {
 		setDisabledButton(true);
 		const email = Cookies.get("email");
 		let response = await SavePhoneNumber(email, phoneNumber.replace(/[()-\s]/g, ''));
@@ -77,12 +89,13 @@ const MFAGetPhoneNumber = ({
 		}
 		setDisabledButton(false);
 	}
+	}
 	const handleToSkipContinue = async () => {
+		if (location?.state) {
 		const email = Cookies.get("email");
 		let mobile = "";
 		let response = await SavePhoneNumber(email, mobile);
 		if (response?.data?.statusCode === 200) {
-			toast.success(response.data?.Message);
 			let mfaResponse = await fetchQuestionMFA(location?.state?.customerEmail);
 			 if (mfaResponse?.data?.statusCode === 200) {
 				navigate("/MFA", { state: { mfaDetails: mfaResponse?.data?.MFAInformation, customerEmail: location?.state?.customerEmail, deviceType: window.navigator.userAgent } });
@@ -93,6 +106,7 @@ const MFAGetPhoneNumber = ({
 		} else {
 			toast.error(response.data?.Message ?? response.data?.errorMessage);
 		}
+	}
 	}
 	//configuring formik
 	const formik = useFormik({
@@ -105,74 +119,107 @@ const MFAGetPhoneNumber = ({
 		navigate(-1);
 	}
 	return (
-		<div data-testid="phoneNumber-container">
-			<Grid>
-				<Grid
-					spacing={1}
-					container
-					item
-					xs={12}
-					sm={12}
-					md={6}
-					lg={5}
-					xl={6}
-					className={classes.twoStepWrap}
-				>
-					<Paper className={classes.twoStepPaper}>
-						<Grid className={classes.headingTextWrap}>
-						<IconButton className={classes.backArrow} onClick={backToVerificationStep}>
-								<ArrowBackIcon className={classes.yellowBackArrow} />
-							</IconButton>
-							<Typography className={classes.twoStepHeading} variant="h5">
-								Let&apos;s set up your phone for 2-Step Verification
-							</Typography>
-						</Grid>
-						<Typography className={classes.twoStepParagraph} style={{ textAlign: 'center' }}>
-							What mobile number do you want to use? We will send a text message to make sure the number is yours.
-						</Typography>
-						<FormControl
-							className={classes.radioButtonwrap}
-							component="fieldset"
-						>
-							<Grid
-								container
-								alignItems="center"
-								item
-								lg={12}
-								md={12}
-								xs={12}
-								className="textBlock"
-								id="phoneInput"
-							>
-								<PhoneNumber
-									name="phone"
-									label="Phone number"
-									placeholder="Enter your phone number"
-									id="phone"
-									type="text"
-									onKeyDown={preventSpace}
-									value={formik.values.phone}
-									onChange={checkMobileNumber}
-									onBlur={formik.handleBlur}
-									error={
-										formik.touched.phone && Boolean(formik.errors.phone)
-									}
-									helperText={formik.touched.phone && formik.errors.phone}
-								/>
-							</Grid>
-						</FormControl>
-						<Typography className={classes.twoStepParagraph} style={{ textAlign: 'center',fontSize:'0.85rem'}}>
-							We will only use this number for account security.
-						</Typography>
-						<Grid className={classes.nextButtonGrid} container >
-							<ButtonPrimary data-testid="skip_button" stylebutton='{"color":""}' onClick={handleToSkipContinue}  className={classes.skip_button} >Skip</ButtonPrimary>
-							<ButtonPrimary data-testid="next_button" stylebutton='{"color":""}' disabled={disabledButton} className={classes.button_space}
-								onClick={handleToSaveContinue} >Next</ButtonPrimary>
-						</Grid>
-					</Paper>
-				</Grid>
-			</Grid>
-		</div>
-	)
+    <div>
+      <CheckLoginTimeout />
+      {loginToken.isLoggedIn && location?.state ? (
+        <>
+          <div data-testid="phoneNumber-container">
+            <Grid>
+              <Grid
+                spacing={1}
+                container
+                item
+                xs={12}
+                sm={12}
+                md={6}
+                lg={5}
+                xl={6}
+                className={classes.twoStepWrap}
+              >
+                <Paper className={classes.twoStepPaper}>
+                  <Grid className={classes.headingTextWrap}>
+                    <IconButton
+                      className={classes.backArrow}
+                      onClick={backToVerificationStep}
+                    >
+                      <ArrowBackIcon className={classes.yellowBackArrow} />
+                    </IconButton>
+                    <Typography className={classes.twoStepHeading} variant="h5">
+                      Let&apos;s set up your phone for 2-Step Verification
+                    </Typography>
+                  </Grid>
+                  <Typography
+                    className={classes.twoStepParagraph}
+                    style={{ textAlign: "center" }}
+                  >
+                    What mobile number do you want to use? We will send a text
+                    message to make sure the number is yours.
+                  </Typography>
+                  <FormControl
+                    className={classes.radioButtonwrap}
+                    component="fieldset"
+                  >
+                    <Grid
+                      container
+                      alignItems="center"
+                      item
+                      lg={12}
+                      md={12}
+                      xs={12}
+                      className="textBlock"
+                      id="phoneInput"
+                    >
+                      <PhoneNumber
+                        name="phone"
+                        label="Phone number"
+                        placeholder="Enter your phone number"
+                        id="phone"
+                        type="text"
+                        onKeyDown={preventSpace}
+                        value={formik.values.phone}
+                        onChange={checkMobileNumber}
+                        onBlur={formik.handleBlur}
+                        error={
+                          formik.touched.phone && Boolean(formik.errors.phone)
+                        }
+                        helperText={formik.touched.phone && formik.errors.phone}
+                      />
+                    </Grid>
+                  </FormControl>
+                  <Typography
+                    className={classes.twoStepParagraph}
+                    style={{ textAlign: "center", fontSize: "0.85rem" }}
+                  >
+                    We will only use this number for account security.
+                  </Typography>
+                  <Grid className={classes.nextButtonGrid} container>
+                    <ButtonPrimary
+                      data-testid="skip_button"
+                      stylebutton='{"color":""}'
+                      onClick={handleToSkipContinue}
+                      className={classes.skip_button}
+                    >
+                      Skip
+                    </ButtonPrimary>
+                    <ButtonPrimary
+                      data-testid="next_button"
+                      stylebutton='{"color":""}'
+                      disabled={disabledButton}
+                      className={classes.button_space}
+                      onClick={handleToSaveContinue}
+                    >
+                      Next
+                    </ButtonPrimary>
+                  </Grid>
+                </Paper>
+              </Grid>
+            </Grid>
+          </div>
+        </>
+      ) : (
+        <CheckLoginStatus />
+      )}
+    </div>
+  );
 }
 export default MFAGetPhoneNumber
