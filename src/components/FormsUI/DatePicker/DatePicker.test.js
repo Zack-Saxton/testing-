d79414b60853bb74f9b523fb2db4b640ee06ff25
ashok2maruthamuthu <ashok.maruthamuthu@zucisystems.com>
2022-07-05 10:20:29 +0530

    Time is modified in test case

--- src/components/FormsUI/DatePicker/DatePicker.test.js
+++ src/components/FormsUI/DatePicker/DatePicker.test.js
@@ -3,7 +3,7 @@ import { cleanup, fireEvent, render, waitFor, act } from '@testing-library/react
 import React from 'react';
 import DatePicker from './index.js';
 
-const SampleDate = "Tue Jan 01 2000 00:03:00 GMT+0530 (India Standard Time)"
+const SampleDate = "Tue Jan 01 2000 12:03:00 GMT+0530 (India Standard Time)"
 
 const component = () => {
   return (
