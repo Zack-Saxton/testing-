dd08665696f8ce950f4c340975e0ed725ee3f0c4
ashok2maruthamuthu <ashok.maruthamuthu@zucisystems.com>
2022-07-21 20:22:55 +0530

    GA4 implementation

--- src/components/Layout/General.js
+++ src/components/Layout/General.js
@@ -6,12 +6,14 @@ import "./Layout.css";
 import Footer from './NormalFooter/NormalFooter';
 import HeaderWithoutMenu from './NormalHeader/HeaderWithoutMenu';
 import Header from './NormalHeader/NormalHeader';
+import GA4 from "../Layout/ga4/GA4";
 
 const General = ({ children, skipHeaderMenu }) => {
 
     const loginToken = JSON.parse(Cookies.get("token") ? Cookies.get("token") : '{ }');
     return (
         <div id="BG">
+            <GA4 />
             {
                 loginToken.isLoggedIn ? <CheckLoginTimeout /> : null
             }
