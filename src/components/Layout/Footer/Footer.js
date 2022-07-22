9f7ebec49fbebea0810ec0734cfa59a5be6076b6
vikas <vidnani@marinerfinance.com>
2022-07-22 12:59:10 -0400

    CA resident link should not navigate into education program link

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -99,7 +99,7 @@ export default function Footer() {
               </div>
               <div className="footer-content">
                 <NavLink data-testid="californiaResident"
-                  to="/californiaResident"
+                  to="/privacyStatement"
                   target="_blank"
                   className="hrefTag"
                 >
