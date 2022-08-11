6326eb129e006543ff50437a2a90d6c03e6df352
Arun Krishnamoorthy <arun.k@zucisystems.com>
2022-08-12 00:35:09 +0530

    reffered to affiliate test case passed

--- src/components/Pages/AffiliatePartner/ReferredFromAffiliate/ReferredFromAffiliateMockData.js
+++ src/components/Pages/AffiliatePartner/ReferredFromAffiliate/ReferredFromAffiliateMockData.js
@@ -1,7 +1,17 @@
 import { useQuery } from "react-query";
 import  { PopulatePartnerReferred } from "../../../Controllers/PartnerSignupController";
+import { useNavigate, useLocation } from "react-router-dom";
 
-export const usePopulatePartnerReferred = (applicantId) => {
+export const usePopulatePartnerReferred = () => {
+  const navigate = useNavigate();
+  const useQueryURL = () => new URLSearchParams(useLocation().search);
+  const query = useQueryURL();
+  const applicantId = query.get("REF");
+
+  if(!applicantId)
+    {
+      navigate("/error")
+    }
   const { data: PopulatePartnerSignupData , isLoading, isError, } = useQuery([ 'populate-data-referred',  applicantId], () => PopulatePartnerReferred( applicantId ));
   return { PopulatePartnerSignupData, isError, isLoading}
 }
