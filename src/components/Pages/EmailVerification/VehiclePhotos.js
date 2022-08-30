b277666f14383b6e8e624b6ae276040be3827105
Prakash-727 <prakash.c@zucisystems.com>
2022-08-30 20:15:02 +0530

    UIUX-text changes in branch portal

--- src/components/Pages/EmailVerification/VehiclePhotos.js
+++ src/components/Pages/EmailVerification/VehiclePhotos.js
@@ -11,20 +11,15 @@ function VehiclePhotos(props) {
   return (
     <Grid data-testid = "vehiclePhotos_component">
       <span className={classes.exampleText}>
-        Click Below To Take Vehicle Pictures If Discussed With Your Loan
-        Officer.
-        <br />
-        Please take clear, wide angle photos from 6 feet away for
-        exterior pictures.
+        Click Below To Upload Vehicle Documents and Photos If Discussed With Your Loan Officer
       </span>
       <Grid item sm={12} md={6} className={classes.nextButton}>
-
         <ButtonPrimary
           stylebutton='{"background": "#FFBC23", "color": "black", "borderRadius": "50px", "margin":"10px 0px"}'
           target="_blank" href={`${ process.env.REACT_APP_GOLDPOINT_HOST_NAME}/?applicationNumber=${ props.applicationNumber }`}
           data-testid = "UploadVehicle_photo"
         >
-          Upload Vehicle Photo
+          Upload Vehicle Information
         </ButtonPrimary>
       </Grid>
     </Grid>
