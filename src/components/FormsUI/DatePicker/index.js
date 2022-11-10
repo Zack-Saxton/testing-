8add3da230b1e02bdfdefd196873a66a63b64bd3
vickeykgv <vignesh.kg@zucisystems.com>
2022-11-11 00:34:59 +0530

    vicky-ops-490 made changes one date picker timeone issue

--- src/components/FormsUI/DatePicker/index.js
+++ src/components/FormsUI/DatePicker/index.js
@@ -15,21 +15,18 @@ import "date-fns";
 import PropTypes from "prop-types";
 import React, { useEffect, useState } from "react";
 import globalMessages from '../../../assets/data/globalMessages.json';
-import { handleDateOffset } from "../../Controllers/CommonController"
 import "../iframe.css";
 
 const DatePickerWrapper = ({ format, label, views,
 	placeholder, required, onChange, disableDate, disablePastDate,
-	maxdate, minyear, error, value, helperText, mask, disableFuture, ...otherProps }) => {
-		
-	
+	maxdate, minyear, error, helperText, value, mask, disableFuture, ...otherProps }) => {
 	const [ selectedDate, setSelectedDate ] = useState(value ?? null);
 	const [ errorTF, setErrorTF ] = useState(false);
 	const [ helperTextTF, setHelperTextTF ] = useState("");
 	useEffect(() => {
-		setSelectedDate(value ? handleDateOffset(new Date(value)) : value);
+		setSelectedDate(value);
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+	}, [ value ]);
 	const handleDateChange = (event) => {
 		setSelectedDate(event);
 		setErrorTF((required && !event.target.value));
