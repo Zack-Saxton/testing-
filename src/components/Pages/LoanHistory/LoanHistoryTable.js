da7bde5159ce9fa5dee33bbb0d827ed67286bec6
Arun Krishnamoorthy <arun.k@zucisystems.com>
2022-11-14 19:39:23 +0530

    Loan history updation

--- src/components/Pages/LoanHistory/LoanHistoryTable.js
+++ src/components/Pages/LoanHistory/LoanHistoryTable.js
@@ -17,6 +17,7 @@ import { NavLink } from "react-router-dom";
 import { useStylesLoanHistory } from "./Style";
 import usrAccountDetails from "../../Controllers/AccountOverviewController"
 import "./Style.css";
+import Typography from "@mui/material/Typography";
 
 export default function LoanHistoryTable() {
 
@@ -25,14 +26,31 @@ export default function LoanHistoryTable() {
   const { isLoading, data: accountDetails } = useQuery('loan-data', usrAccountDetails);
   //View part
   return (
-    <Grid item xs={12} className={classes.gridRecordTable} data-testid="with_Data">
+    <div style = {{width : "100%"}}>
+      <Grid
+            container
+            item
+            direction="row"
+            xs={12}
+            className={classes.activeLoanWrap}
+            style = {{paddingBottom : "20px"}}
+          >
+            <Typography
+              variant="h5"
+              className={classes.subheading}
+              data-testid="active loans"
+            >
+              Active Loan
+            </Typography>
+          </Grid>
+    <Grid item xs={12}  style = {{paddingBottom : "20px"}}  data-testid="with_Data">
       <TableContainer component={Paper}>
         <Table aria-label="simple table">
           <TableHead>
             <TableRow>
               <TableCell className={classes.tableHead} align="left" >Account Number</TableCell>
               <TableCell className={classes.tableHead} align="left" >Date Opened</TableCell>
-              <TableCell className={classes.tableHead} align="left" >Date Closed</TableCell>
+              {/* <TableCell className={classes.tableHead} align="left" >Date Closed</TableCell> */}
               <TableCell className={classes.tableHeadLast} align="right" >Amount Financed</TableCell>
               <TableCell className={classes.tableHeadLast2} align="center" >Documents</TableCell>
             </TableRow>
@@ -48,7 +66,7 @@ export default function LoanHistoryTable() {
                 <TableRow key={row.loanData.accountNumber} >
                   <TableCell component="th" className={classes.tableHeadRow} scope="row" align="left">{row.loanData.accountNumber}</TableCell>
                   <TableCell className={classes.tableHeadRow} align="left" >{row.loanData?.loanOriginationDate ? Moment(row.loanData.loanOriginationDate).format("MM/DD/YYYY") : ''}</TableCell>
-                  <TableCell className={classes.tableHeadRow} align="left" >{row.loanData.dueDate ? Moment(row.loanData.dueDate).format("MM/DD/YYYY") : ''}</TableCell>
+                  {/* <TableCell className={classes.tableHeadRow} align="left" >{row.loanData.dueDate ? Moment(row.loanData.dueDate).format("MM/DD/YYYY") : ''}</TableCell> */}
                   <TableCell className={classes.tableHeadRowLast} align="right" >
                     {
                       <NumberFormat value={row.loanPaymentInformation?.accountDetails?.OriginalFinancedAmount ? Math.abs(row.loanPaymentInformation.accountDetails.OriginalFinancedAmount) : ''} displayType={'text'} thousandSeparator={true} decimalScale={2} fixedDecimalScale={true} prefix={'$'} />
@@ -75,5 +93,78 @@ export default function LoanHistoryTable() {
         </Table>
       </TableContainer>
     </Grid>
+
+    <Grid
+            container
+            item
+            direction="row"
+            xs={12}
+            className={classes.activeLoanWrap}
+            style = {{paddingBottom : "20px"}}
+          >
+            <Typography
+              variant="h5"
+              className={classes.subheading}
+              data-testid="active loans"
+            >
+              Closed Loan
+            </Typography>
+          </Grid>
+<Grid item xs={12} className={classes.gridRecordTable} data-testid="with_Data">
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell className={classes.tableHead} align="left" >Account Number</TableCell>
+              <TableCell className={classes.tableHead} align="left" >Date Opened</TableCell>
+              <TableCell className={classes.tableHead} align="left" >Date Closed</TableCell>
+              {/* <TableCell className={classes.tableHeadLast} align="right" >Amount Financed</TableCell> */}
+              <TableCell className={classes.tableHeadLast2} align="center" >Documents</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {isLoading && (
+              <TableRow>
+                <TableCell colSpan="7" align="center" data-testid="while_Loading"><CircularProgress /></TableCell>
+              </TableRow>
+            )}  
+            { accountDetails?.data?.loanData?.length && (
+              accountDetails?.data?.loanData.map((row) => (
+                row.status === "Closed" ?
+                <TableRow key={row.accountNumber} >
+                  <TableCell component="th" className={classes.tableHeadRow} scope="row" align="left">{row.accountNumber}</TableCell>
+                  <TableCell className={classes.tableHeadRow} align="left" >{row?.loanOriginationDate ? Moment(row.loanOriginationDate).format("MM/DD/YYYY") : ''}</TableCell>
+                  <TableCell className={classes.tableHeadRow} align="left" >{row.status === "Closed" ? row.dueDate ? Moment(row.dueDate).format("MM/DD/YYYY") : '' : ''}</TableCell>
+                  {/* <TableCell className={classes.tableHeadRowLast} align="right" >
+                    {
+                      <NumberFormat value={row.loanPaymentInformation?.accountDetails?.OriginalFinancedAmount ? Math.abs(row.loanPaymentInformation.accountDetails.OriginalFinancedAmount) : ''} displayType={'text'} thousandSeparator={true} decimalScale={2} fixedDecimalScale={true} prefix={'$'} />
+                    }
+                  </TableCell> */}
+                  <TableCell className={classes.loanDocumentsTableCell} align="center">
+                    <NavLink to="/customers/loanDocument" state={{ accNo: row.accountNumber }} className={classes.textDecoration}>
+                      <Tooltip title="View Loan Documents" placement="top">
+                        <FindInPageIcon className={classes.findInPageIcon} />
+                      </Tooltip>
+                    </NavLink>
+                  </TableCell>
+                </TableRow> : ""
+              ))
+            )} 
+            { 
+           !isLoading && !accountDetails?.data?.loanData?.length && (
+              <TableRow data-testid="while_Error">
+                <TableCell colSpan="7" align="center" >You do not have any Closed loans</TableCell>
+              </TableRow>
+           )
+            }
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Grid>
+
+    </div>
+
+
+
   );
 }
