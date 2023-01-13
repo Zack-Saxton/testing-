dc83168c0fcc89ed65d7f1f07b7bdcbe66d2e520
vickeykgv <vignesh.kg@zucisystems.com>
2023-01-13 18:27:30 +0530

    vicky-cac-uiux-3167 Added some test cases that improves the code coverage.

--- src/components/Pages/Login/ResetPassword.test.js
+++ src/components/Pages/Login/ResetPassword.test.js
@@ -4,8 +4,10 @@ import "@testing-library/jest-dom/extend-expect";
 import { act, fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
 import ResetPassword from "./ResetPassword";
+import ResetPasswordController from "../../Controllers/ResetPasswordController";
+
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -19,11 +21,40 @@ const queryClient = new QueryClient({
 const theme = createTheme();
 window.scrollTo = jest.fn();
 
+jest.mock('../../Controllers/ResetPasswordController', () => {
+	const mockedData = {
+    "status": 200,
+    "statusText": "OK",
+    "data": {
+        "passwordReset": true,
+        "error": true
+    }
+}
+	return {
+		ResetPasswordController: jest.fn((password) => {
+					// define the mock function's behavior
+					return mockedData;
+			}),
+	};
+});
+
 const component = () => {
 	return (
 		<ThemeProvider theme={theme}>
 			<QueryClientProvider client={queryClient}>
-				<BrowserRouter>
+				<BrowserRouter initialEntries={[{ pathname: '/', state: {Email : "vickeykgv@gmail.com", flag:true}} ]}>
+					<ResetPassword />
+				</BrowserRouter>
+			</QueryClientProvider>
+		</ThemeProvider>
+	);
+}
+
+const RenderComponent = () => {
+	return (
+		<ThemeProvider theme={theme}>
+			<QueryClientProvider client={queryClient}>
+				<BrowserRouter initialEntries={[{ pathname: '/', state: {Email : "vickeykgv@gmail.com", flag:true}} ]}>
 					<ResetPassword />
 				</BrowserRouter>
 			</QueryClientProvider>
@@ -167,13 +198,63 @@ test("Check Password Match", async () => {
 
 test("Button Onclick", async () => {
 	render(component());
-	const button = screen.getByTestId("submit");
+	const button = screen.getByTestId("submitButton");
 	await act(() => {
 		fireEvent.click(button);
 	});
+});
+
+test("Fill form Button Onclick", async () => {
+	// render(component());
+	const { container } = render(component());
+	const input = container.querySelector(`input[name="email"]`);
+	await act(() => {
+		fireEvent.change(input, { target: { value: "mariner@gmail.com" } });
+	});
+	const passwordElement = container.querySelector(`input[name="password"]`);
+	expect(passwordElement.hasAttribute("name")).toBe(true);
+	await act(() => {
+		fireEvent.change(passwordElement, { target: { value: "Test@123" } });
+	});
+	expect(passwordElement.value).toBe("Test@123");
+	const confirmPasswordElement = container.querySelector(`input[name="confirmPassword"]`);
+	expect(confirmPasswordElement.hasAttribute("name")).toBe(true);
+	await act(() => {
+		fireEvent.change(confirmPasswordElement, { target: { value: "Test@123" } });
+	});
+	expect(passwordElement.value).toBe("Test@123");
+	const button = screen.getByText("Reset Password");
+	await act(() => {
+		fireEvent.click(button);
+	});
+});
+
+test("Preventing space in the password field", async () => {
+
+	const { container } = render(component());
+  const input = container.querySelector(`input[name="password"]`);
+  expect(input).toBeTruthy();
+  fireEvent.keyDown(input, 32);
+  expect(input.value).toBe("");
 });
 
 test('Should match the snapshot', () => {
 	const { asFragment } = render(component());
 	expect(asFragment).toMatchSnapshot();
 })
+
+describe('ResetPassword', () => {
+  test('submitting form with valid password and confirm password', async () => {
+		const { container } = render(component());
+    const emailInput = container.querySelector(`input[name="email"]`);
+    const passwordInput = container.querySelector(`input[name="password"]`);
+    const confirmPasswordInput = container.querySelector(`input[name="confirmPassword"]`);
+    const submitButton = screen.getByText("Reset Password");
+    fireEvent.change(emailInput, { target: { value: 'vickeykgv@gmail.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'password1@A' } });
+    fireEvent.change(confirmPasswordInput, { target: { value: 'password1@A' } });
+		await act(() => {
+    	fireEvent.click(submitButton);
+		});
+  });
+});
